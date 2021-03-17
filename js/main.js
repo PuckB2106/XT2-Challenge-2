42a6c69aabf2abfe4eb83a25627785acbe557afd
PuckB2106 <78908678+PuckB2106@users.noreply.github.com>
2021-03-17 12:27:38 +0100

    grid

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
 var today = new Date();
 
-document.getElementById('datum').innerHTML = today.getDate() + '/' + (today.getMonth()+1);
-// This Month
+var maandenVanHetJaar = new Array('januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december');
 
-var maanden = new Array('Januari', 'Debruari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December');
+// dit geeft de dag, maand en het jaar weer
+document.getElementById('tijdA').innerHTML = today.getDate()  + ' ' +  maandenVanHetJaar[today.getMonth()] + ' ' +  today.getFullYear();;
 
-document.getElementById('datum').innerHTML = today.getDate() + ' ' + maanden[today.getMonth()] + ' ' + today.getFullYear();
+//functie waarmee de huidige tijd wordt weergegeven in uren, minuten en seconden
+function huidigeTijd(){
+  var today = new Date();
+
+  var seconds = addLeadingZero(today.getSeconds());
+  var minutes = addLeadingZero(today.getMinutes());
+
+  document.getElementById('tijdL').innerHTML = today.getHours() + ':' +  minutes +  ':' + seconds ;
+
+  // functie voegt 0 toe
+  function addLeadingZero(number) {
+      if(number < 10){
+          return '0' + number;
+      }
+      return number
+  }
+}
+
+//variabele tellen mee
+var timevar = setInterval(huidigeTijd, 1000);
